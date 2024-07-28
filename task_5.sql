39866b896504b709289bffa3d690ec68cf9735c7
thecollekta <festus233@gmail.com>
2024-07-28 22:51:05 +0000

    A script to insert a single row in the table customer (database alx_book_store) in your MySQL server.

--- task_5.sql
+++ task_5.sql
@@ -5,9 +5,4 @@ USE alx_book_store;
 
 -- Insert a single row into customers table
 INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES (
-    1,
-    'Cole Baidoo',
-    'cbaidoo@sandtech.com',
-    '123 Happiness Ave.'
-    );
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
