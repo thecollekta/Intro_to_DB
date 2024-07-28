0a0d79e226aa76d2fac1700d09799a87a3fb600c
Collekta <festus233@gmail.com>
2024-07-28 11:29:25 +0000

    Designed a MySQL database for an online bookstore.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -41,6 +41,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
